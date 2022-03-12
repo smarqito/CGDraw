b79022d02da56e0f20219804eacd6e19597d674e
José Malheiro <jafmalheiro@live.com.pt>
2022-03-12 20:20:56 +0000

    add: main with args + writes on xml done

---
+++ generator/writer.h
@@ -0,0 +1,10 @@
+#include <tinyxml2.h>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <GL/glut.h>
+#include <math.h>
+#include "shapes.h"
+
+using namespace std;
+using namespace tinyxml2;
